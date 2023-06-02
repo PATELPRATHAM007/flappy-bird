6bef8e876087b007983b7c0316fe1641c171eff2
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-06-02 18:15:44 +0530

    added opening panel

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,17 @@
 public class Game {
     public static void main(String[] args) {
-        Panel panel = new Panel();
-        new Window(panel);
+        openingPanel openingPanel = new openingPanel();
+        Window openingWindow = new Window(openingPanel);
+        openingWindow.setTitle("welcome to flappy-bird");
+        try {
+            Thread.sleep(1582);
+        }
+        catch (Exception e) {
+            System.out.println("failed sleep for 1.582 seconds.");
+        }
+        openingWindow.dispose();
+        new Window(openingPanel);
+
+
     }
 }
